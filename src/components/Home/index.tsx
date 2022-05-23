c1dcfec6362e2aeb4b855a28584c4bd667a691a1
kenthow@gmail.com <kenthow@gmail.com>
2022-05-23 17:00:14 -0400

    small change to wording in homepage

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () =>  {
     <div className='content'>
       <div className='img'>
         <div>
-          <h1 className='txt title'>Home</h1>
+          <h1 className='txt title'>Kent Howard</h1>
           <h2 className='txt sub-title'>This is the home/landing page</h2>
           <article className='txt body-txt'>Some sort of information will be going here</article>
         </div>
